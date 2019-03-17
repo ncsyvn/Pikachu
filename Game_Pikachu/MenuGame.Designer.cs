96ae2d14db5f1c4a53b1c052b5aff94829cc4d31
HoangDan <danhoangvandan.dan@gmail.com>
2019-03-17 07:54:22 +0700

    Add form Menu game

---
+++ Game_Pikachu/MenuGame.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Game_Pikachu
+{
+    partial class MenuGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MenuGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 629);
+            this.Name = "MenuGame";
+            this.Text = "MenuGame";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
